e106d2eac1e227182365e15b27ebc3b48ca7bf72
yaseenjohnston <217095259@mycput.ac.za>
2020-07-04 20:56:17 +0200

    Constructors added to Employee and Job entities. EmployeeFactory and …

--- src/main/java/com/furnitureapp/factory/EmployeeFactory.java
+++ src/main/java/com/furnitureapp/factory/EmployeeFactory.java
@@ -10,7 +10,7 @@ import com.furnitureapp.utility.Helper;
 
 public class EmployeeFactory {
 
-    public static Employee createEmployee(String fullName, String address, String emailAddress, String phone, String shift, double salary){
+    public static Employee createEmployee(String fullName, String address, String emailAddress, String phone, String shift, double salary, String passCode){
 
         Employee employee = new Employee.EmployeeBuilder()
                 .setEmployeeCode(Helper.generateCode())
@@ -20,6 +20,7 @@ public class EmployeeFactory {
                 .setPhone(phone)
                 .setShift(shift)
                 .setSalary(salary)
+                .setPassCode(passCode)
                 .build();
         return employee;
     }
