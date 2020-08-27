e3e61753d31285463c8360d6e7bc25408551ed04
yaseenjohnston <217095259@mycput.ac.za>
2020-08-27 22:03:00 +0200

    Impl for Staff Repository and test cases for Staff Repository

--- src/test/java/com/furnitureapp/repository/staff/impl/EmployeeRepositoryImplTest.java
+++ src/test/java/com/furnitureapp/repository/staff/impl/EmployeeRepositoryImplTest.java
@@ -4,12 +4,15 @@ import com.furnitureapp.entity.staff.Employee;
 import com.furnitureapp.factory.staff.EmployeeFactory;
 import com.furnitureapp.factory.staff.JobFactory;
 import com.furnitureapp.repository.staff.EmployeeRepository;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.util.Set;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class EmployeeRepositoryImplTest {
 
     private static EmployeeRepository repo = EmployeeRepositoryImpl.getEmployeeRepository();
@@ -19,31 +22,31 @@ public class EmployeeRepositoryImplTest {
             JobFactory.createJob("Software Engineer", "Develops and maintains software infrastructure"));
 
     @Test
-    public void create() {
+    public void a_create() {
         Employee created = repo.create(employee);
         assertEquals(created, employee);
     }
 
     @Test
-    public void read() {
+    public void b_read() {
         Employee read = repo.read(employee.getEmployeeCode());
-        assertEquals(read.getEmployeeCode(), employee.getEmployeeCode());
+        assertEquals(employee.getEmployeeCode(), read.getEmployeeCode());
     }
 
     @Test
-    public void update() {
+    public void c_update() {
         Employee updated = new Employee.EmployeeBuilder().copy(employee).setFullName("Isaac Isaacs").build();
         updated = repo.update(updated);
         assertNotEquals(updated, employee);
     }
 
     @Test
-    public void delete() {
+    public void d_delete() {
         repo.delete(employee.getEmployeeCode());
     }
 
     @Test
-    public void list() {
+    public void e_list() {
         Set<Employee> employees = repo.list();
         assertEquals(0, employees.size());
     }
--- src/test/java/com/furnitureapp/repository/staff/impl/JobRepositoryImplTest.java
+++ src/test/java/com/furnitureapp/repository/staff/impl/JobRepositoryImplTest.java
@@ -3,43 +3,46 @@ package com.furnitureapp.repository.staff.impl;
 import com.furnitureapp.entity.staff.Job;
 import com.furnitureapp.factory.staff.JobFactory;
 import com.furnitureapp.repository.staff.JobRepository;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.util.Set;
 
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class JobRepositoryImplTest {
 
     private static JobRepository repo = new JobRepositoryImpl();
     private static Job job = JobFactory.createJob("Software Engineer", "Develops and maintains software infrastructure");
 
     @Test
-    public void create() {
+    public void a_create() {
         Job created = repo.create(job);
         assertEquals(job, created);
     }
 
     @Test
-    public void read() {
+    public void b_read() {
         Job read = repo.read(job.getJobCode());
         assertEquals(job.getJobCode(), read.getJobCode());
     }
 
     @Test
-    public void update() {
+    public void c_update() {
         Job updated = new Job.JobBuilder().copy(job).setJobCode((long) 465789).build();
         updated = repo.update(updated);
         assertNotEquals(updated.getJobCode(), job.getJobCode());
     }
 
     @Test
-    public void delete() {
+    public void d_delete() {
         repo.delete(job.getJobCode());
     }
 
     @Test
-    public void list() {
+    public void e_list() {
         Set<Job> jobs = repo.list();
         assertEquals(0, jobs.size());
     }
