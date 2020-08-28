4353f236076f448d726fa909b5563bb3f2e00ff0
Owin Ntumba <49818109+winaowin@users.noreply.github.com>
2020-08-28 02:41:38 +0200

    New PromotionRepository

--- src/main/java/com/furnitureapp/repository/sales/CartRepository.java
+++ src/main/java/com/furnitureapp/repository/sales/CartRepository.java
@@ -1,7 +1,22 @@
+/*
+Author: Ntumba Owin
+Date of Creation: 27/8/2020
+Desc:This is the CartRepositoryImpl class, it contains the CRUD method for the Cart entity.
+     Carts (Cart class objects) will not be saved in any database.
+     They will just exist in the browser using sessions (creation, update, & deletion).
+ */
 package com.furnitureapp.repository.sales;
 
 import com.furnitureapp.entity.sales.Cart;
 import com.furnitureapp.repository.IRepository;
 
+import java.util.Set;
+
 public interface CartRepository extends IRepository<Cart, Long> {
+
+    //CRUD method that will be apply to the cart objects
+
+    //Creation of the set that will contain the Cart objects and on which the CRUD method will be applied
+
+
 }
--- src/main/java/com/furnitureapp/repository/sales/impl/CartRepositoryImpl.java
+++ src/main/java/com/furnitureapp/repository/sales/impl/CartRepositoryImpl.java
@@ -1,3 +1,10 @@
+/*
+Author: Ntumba Owin
+Date of Creation: 27/8/2020
+Desc:This is the CartRepository interface, it contains the data type that will be use to store temporally the cart.
+     Carts (Cart class objects) will not be saved in any database.
+     They will just exist in the browser using sessions (creation, update, & deletion).
+ */
 package com.furnitureapp.repository.sales.impl;
 
 public class CartRepositoryImpl {
