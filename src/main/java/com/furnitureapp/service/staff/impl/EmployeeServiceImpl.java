dfe114dd24f9126ae77287b29be6d8238782a9a5
yaseenjohnston <217095259@mycput.ac.za>
2020-09-03 09:16:45 +0200

    Staff Services and Tests completed

--- src/main/java/com/furnitureapp/service/staff/impl/EmployeeServiceImpl.java
+++ src/main/java/com/furnitureapp/service/staff/impl/EmployeeServiceImpl.java
@@ -1,4 +1,51 @@
 package com.furnitureapp.service.staff.impl;
 
-public class EmployeeServiceImpl {
+import com.furnitureapp.entity.staff.Employee;
+import com.furnitureapp.repository.staff.EmployeeRepository;
+import com.furnitureapp.repository.staff.impl.EmployeeRepositoryImpl;
+import com.furnitureapp.service.staff.EmployeeService;
+
+
+import java.util.Set;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository repository;
+    private static EmployeeService employeeService = null;
+
+    private EmployeeServiceImpl() {
+        this.repository = EmployeeRepositoryImpl.getEmployeeRepository();
+    }
+
+    public static EmployeeService getEmployeeService(){
+        if(employeeService == null){
+            employeeService = new EmployeeServiceImpl();
+        }
+        return employeeService;
+    }
+
+    @Override
+    public Set<Employee> list() {
+        return this.repository.list();
+    }
+
+    @Override
+    public Employee create(Employee employee) {
+        return this.repository.create(employee);
+    }
+
+    @Override
+    public Employee read(Integer employeeCode) {
+        return this.repository.read(employeeCode);
+    }
+
+    @Override
+    public Employee update(Employee employee) {
+        return this.repository.update(employee);
+    }
+
+    @Override
+    public void delete(Integer employeeCode) {
+        this.repository.delete(employeeCode);
+    }
 }
