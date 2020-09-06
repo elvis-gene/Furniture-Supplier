4bbba2e72b093bf294755eccd7aa0ca11a6bd5fb
DIgitalNash <217161774@mycput.ac.za>
2020-09-06 22:01:35 +0200

    Delivery Service Munashe

--- src/main/java/com/furnitureapp/service/distribution/impl/DeliveryServiceImpl.java
+++ src/main/java/com/furnitureapp/service/distribution/impl/DeliveryServiceImpl.java
@@ -1,4 +1,51 @@
 package com.furnitureapp.service.distribution.impl;
 
-public class DeliveryServiceImpl {
+import com.furnitureapp.entity.distribution.Delivery;
+import com.furnitureapp.repository.distribution.DeliveryRepository;
+import com.furnitureapp.repository.distribution.impl.DeliveryRepositoryImpl;
+import com.furnitureapp.service.distribution.DeliveryService;
+
+
+import java.util.Set;
+
+public class DeliveryServiceImpl implements DeliveryService {
+    private static DeliveryService deliveryService = null;
+    private DeliveryRepository deliveryRepository;
+
+    public DeliveryServiceImpl(){
+        this.deliveryRepository = DeliveryRepositoryImpl.getDeliveryRepository();
+    }
+
+    public static DeliveryService getDeliveryService() {
+        if (deliveryService == null)
+            deliveryService = new DeliveryServiceImpl();
+        return deliveryService;
+    }
+
+    @Override
+    public Set<Delivery> list() {
+        return deliveryRepository.list();
+    }
+
+    @Override
+    public Delivery create(Delivery delivery) {
+        return deliveryRepository.create(delivery);
+    }
+
+    @Override
+    public Delivery read(Integer deliveryCode) {
+        return deliveryRepository.read(deliveryCode);
+    }
+
+    @Override
+    public Delivery update(Delivery delivery) {
+        return deliveryRepository.update(delivery);
+    }
+
+    @Override
+    public boolean delete(Integer deliveryCode) {
+        return deliveryRepository.delete(deliveryCode);
+    }
+
+
 }
