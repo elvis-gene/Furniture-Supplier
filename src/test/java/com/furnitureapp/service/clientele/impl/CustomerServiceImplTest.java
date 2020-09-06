debf388585f6812fba7bb5431d6f1cf24cad6972
nkosi <nkosi@globee.com>
2020-09-06 23:19:26 +0200

    Add a service implementation for Customer and Appointment entity with tests

--- src/test/java/com/furnitureapp/service/clientele/impl/CustomerServiceImplTest.java
+++ src/test/java/com/furnitureapp/service/clientele/impl/CustomerServiceImplTest.java
@@ -1,7 +1,57 @@
 package com.furnitureapp.service.clientele.impl;
-
+/*
+ * @author: @nkosinathisola
+ * Description: Customer Service Impl Tests
+ */
+import com.furnitureapp.entity.clientele.Customer;
+import com.furnitureapp.factory.clientele.CustomerFactory;
+import com.furnitureapp.service.clientele.CustomerService;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import java.util.Set;
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CustomerServiceImplTest {
+    private static CustomerService service = CustomerServiceImpl.getCustomerService();
+    private static Customer customer = CustomerFactory.createCustomer("Nkosinathi Sola",
+            "nkosinathisola@gmail.com",
+            "0825173224",
+            "24 Lancaster Street Claremont, Cape Town RSA",
+            "password123");
 
+    @Test
+    public void a_create_customer() {
+        Customer created = service.create(customer);
+        assertEquals(created, customer);
+        System.out.println("Create: " + created);
+    }
+
+    @Test
+    public void b_read_customer() {
+        Customer read = service.read(customer.getCustomerCode());
+        assertEquals(read.getCustomerCode(), customer.getCustomerCode());
+        System.out.println("Read: " + read);
+    }
+
+    @Test
+    public void c_update_customer() {
+        Customer updated = new Customer.CustomerBuilder().copy(customer).setFullName("Emanuel Skye").build();
+        updated = service.update(updated);
+        assertNotEquals(updated, customer);
+        System.out.println("Update: " + updated);
+    }
+
+    @Test
+    public void d_delete_customer() {
+        boolean deleted = service.delete(customer.getCustomerCode());
+        assertTrue(deleted);
+    }
+
+    @Test
+    public void list() {
+        Set<Customer> customers = service.list();
+        assertEquals(0, customers.size());
+    }
 }
