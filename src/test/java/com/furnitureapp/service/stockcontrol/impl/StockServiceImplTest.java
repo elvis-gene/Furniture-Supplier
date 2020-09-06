12cfc7595039aade68fbcfd63cb3e7aaa8cd24e7
anwar-albandar <amamb201000@gmail.com>
2020-09-06 21:41:18 +0200

    Stock services

--- src/test/java/com/furnitureapp/service/stockcontrol/impl/StockServiceImplTest.java
+++ src/test/java/com/furnitureapp/service/stockcontrol/impl/StockServiceImplTest.java
@@ -1,7 +1,52 @@
 package com.furnitureapp.service.stockcontrol.impl;
 
+import com.furnitureapp.entity.stockcontrol.Stock;
+import com.furnitureapp.factory.stockcontrol.StockFactory;
+import com.furnitureapp.service.stockcontrol.StockService;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StockServiceImplTest {
+    private static StockService service = StockServiceImpl.getService();
+    // TODO: With database, input product category code.
+    private static Stock stock = StockFactory.createStock(89);
 
+    @Test
+    public void list() {
+        Set<Stock> stocks = service.list();
+        assertEquals(0, stocks.size());
+    }
+
+    @Test
+    public void a_create() {
+        Stock created = service.create(stock);
+        assertEquals(stock, created);
+        System.out.println("New Stock: "+ created);
+    }
+
+    @Test
+    public void b_read() {
+        Stock readStock = service.read(stock.getProdCatCode());
+        assertEquals(stock, readStock);
+        System.out.println("Read stock: "+ readStock);
+    }
+
+    @Test
+    public void c_update() {
+        Stock updatedStock = new Stock.StockBuilder().copy(stock).setQuantity(78).build();
+        updatedStock = service.update(updatedStock);
+        System.out.println("Updated stock: "+ updatedStock);
+    }
+
+    @Test
+    public void d_delete() {
+        boolean deleted = service.delete(stock.getProdCatCode());
+        assertTrue(deleted);
+    }
 }
